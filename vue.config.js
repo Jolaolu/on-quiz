7ee92756fc2326f22de8bc26358fd8002eb7a868
Anjolaoluwa <adebayoanjola@gmail.com>
2022-07-26 16:33:24 +0200

    add font and scss config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ var path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 const vueSrc = './src'
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/assets/scss/main.scss";',
+      },
+    },
+  },
   configureWebpack: {
     resolve: {
       alias: {
